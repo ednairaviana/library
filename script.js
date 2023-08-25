093c18c4701b1f9a8f9a5c778b60fa39a1ab7e08
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-08-25 15:40:27 -0300

    Create functions for main-form

--- script.js
+++ script.js
@@ -27,3 +27,55 @@ function isRead(index) {
         library[index].read = true;
     }
 }
+
+const form = (function() {
+    const ttlInput = document.querySelector("#title");
+    const authorInput = document.querySelector("#author");
+    const pagesInput = document.querySelector("#pages");
+    const button = document.querySelector("#form-btn");
+
+    button.addEventListener("click", () => {
+        if (validateForm() == true) {
+            addBook(ttlInput.value, authorInput.value, pagesInput.value, setRead());
+            clearInput();
+            domFunctions.renderBooks();
+        }
+    })
+
+    function validateForm() {
+        const spanTitle = document.querySelector("#span-title");
+        const spanAuthor = document.querySelector("#span-author");
+        const spanPages = document.querySelector("#span-pages");
+    
+        spanTitle.innerText = "";
+        spanAuthor.innerText = "";
+        spanPages.innerText = "";
+    
+        if (ttlInput.value == "") {
+            spanTitle.innerText = "What's the title?"
+            return false;
+        } else if (authorInput.value == "") {
+            spanAuthor.innerText = "Who's the author?"
+            return false;
+        } else if (pagesInput.value == "") {
+            spanPages.innerText = "How many pages?"
+            return false
+        } else {
+            return true;
+        }
+    }
+
+    function clearInput() {
+        ttlInput.value = "";
+        authorInput.value = "";
+        pagesInput.value = "";
+    }
+
+    function setRead() {
+        if(document.getElementById("read").checked) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+})()
